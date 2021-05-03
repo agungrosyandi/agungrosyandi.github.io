73699e79f1d0671f78573dc74f77a2fc62b5f9ef
agung <73769474+agungrosyandi@users.noreply.github.com>
2021-05-04 03:06:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    // navbar sticky script
+
+    $(window).scroll(function() {
+        if(this.scrollY > 5) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+    });
+
+    
+    // navbar hiden script
+
+    var lastScrollTop = 0;
+        navbar = document.getElementById("navbar");
+        window.addEventListener("scroll", function(){
+            var scrollTop = window.pageYOffset || document
+                .documentElement.scrollTop;
+                if (scrollTop > lastScrollTop){
+                    navbar.style.top = "-150px";
+                } else {
+                    navbar.style.top = "0";
+                }
+                lastScrollTop = scrollTop;
+        })
+
+    // typing Animation script
+
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer Enthusiast", "UI/UX Enthusiast", "World Politics Enthusiast", "Historical Enthusiast"],
+        typedSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+});
